156f4d6ef5c57011e113696a6480fec9d70a5a77
Mustafo7777 <mustafoali7777@gmail.com>
2022-11-19 20:33:04 +0500

    Added first kescha

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Implicit casting
+//short=>int=>long=>float=>double=>decimal
+//short age = 354;
+//decimal bigAge= age;
+//Console.WriteLine(bigAge);
+//deciAge.ToString()
+//decimal deciAge = age;
+//System.Console.WriteLine(deciAge.ToString());
+//explicit casting
+//int floraType = 32323;
+//short smallFloraType=(short)floraType;
+//Console.WriteLine(smallFloraType);
+//Convert.ToInt32()
+//short randomNuber= Convert.ToInt16(26.25);
+//System.Console.WriteLine(deciAge.ToString());
+System.Console.Write("Ismingizni kiriting : ");
+string name = System.Console.ReadLine();
+string greeting = $"Salom, {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Yoshingizni kiriting : ");
+string ageAsString = System.Console.ReadLine();
+int age =Convert.ToInt32(ageAsString);
+System.Console.Write("Keschani yoshini kiriting : ");
+string keschasAgeAsString =  System.Console.ReadLine();
+int keschasAge = Convert.ToInt32(keschasAgeAsString);
+System.Console.WriteLine("Ayiruv amalga oshirilmoqda...");
+
+System.Console.WriteLine($"Muvaffaqiyatli amalga oshirildi!!! Sizning yoshongiz va Keschaning yoshodagi farq {age - keschasAge} ekan");
+
+
