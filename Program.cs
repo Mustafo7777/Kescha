be072b141a5c08ac86522ccdfa93ec10efa6b808
Mustafo7777 <118614117+Mustafo7777@users.noreply.github.com>
2022-11-20 19:53:36 +0500

    Added Do while loop

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
-﻿System.Console.Write("Ismingizni kiriting : ");
-string name = System.Console.ReadLine();
-string greeting = $"Salom, {name}";
-System.Console.WriteLine(greeting);
+﻿string password ="";
+do{
+  System.Console.WriteLine("kodni kiriting :");
+  password= Console.ReadLine();
+}
+while (password != "1111");
 
-System.Console.Write("Ali yoshini kiriting : ");
-string alisAgeAsString = System.Console.ReadLine();
-int alisAge =Convert.ToInt32(alisAgeAsString);
-System.Console.Write("Valini yoshini kiriting : ");
-string valisAgeAsString =  System.Console.ReadLine();
-int valisAge = Convert.ToInt32(valisAgeAsString);
-if(alisAge<valisAge)
-{
-  System.Console.WriteLine("Ali kichkina Validan");
-}
-else if(alisAge==valisAge)
-{
-  System.Console.WriteLine("Ali bilan Vali teng");
-}
-else 
-{
-    System.Console.WriteLine("Ali katta Validan");
-}
+System.Console.Write("Ismingizni kiriting : ");
+string name = System.Console.ReadLine();
+string greeting = $"Salom, {name}, Kescha dasturiga xush kelibsiz!";
+System.Console.WriteLine(greeting);
